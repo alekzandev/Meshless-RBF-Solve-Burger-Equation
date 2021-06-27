e8da8712575c52c679f117a3d23944df900a7730
Hausdorff94 <jhonyal58@gmail.com>
2021-06-26 23:28:34 -0500

    Update

--- function_Fm.py
+++ function_Fm.py
@@ -21,8 +21,8 @@ poly_b = np.array([[-1, -1, 1], [1/2, 3/2, -1], [3/2, 1/8, -3/8]])
 x = np.array([0.16, .093])
 
 my_obj = assembled_matrix(Mi, Mb, x, 2, 1, poly_b=poly_b)
-X0 = my_obj.X_0()
-print(my_obj.F_m(X0))
+#X0 = my_obj.X_0()
+#print(my_obj.F_m(X0))
 xn = Mi[0,:]
-my_obj.x = xn
-print(my_obj.F_m(X0))
+#my_obj.x = xn
+#print(my_obj.F_m(X0))
