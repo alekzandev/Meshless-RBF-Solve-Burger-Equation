c33d52882ad1a14350d32addea6697fa54a485d2
Johny Vallejo <jhonyal58@gmail.com>
2021-07-21 19:44:31 -0500

    Update

--- explicit_RK.py
+++ explicit_RK.py
@@ -2,29 +2,30 @@ import numpy as np
 
 
 class explicit_RungeKutta(object):
-    def __init__(self, f, u0, t0, te, N):
+    def __init__(self, f, u0, t0, te, N, uh):
         self.f = f
         self.u0 = u0
         self.dt = (te-t0)/(N+2)
         self.timegrid = np.linspace(t0, te, N+2)
+        self.uh = uh
 
     def stage2(self, t):
-        return self.u0 + self.dt/2 * self.f(t + self.dt/2, self.u0)
+        return self.u0 + self.dt/2 * self.f(t + self.dt/2, self.u0, self.uh)
 
     def stage3(self, t):
         ub = self.stage2(t)
-        return self.u0 + self.dt/2 * self.f(t + self.dt/2, ub)
+        return self.u0 + self.dt/2 * self.f(t + self.dt/2, ub, self.uh)
 
     def stage4(self, t):
         ub = self.stage3(t)
-        return self.u0 + self.dt * self.f(t + self.dt, ub)
+        return self.u0 + self.dt * self.f(t + self.dt, ub, self.uh)
 
     def stagef(self, t):
         return self.u0\
-            + self.dt/6 * (self.f(t, self.u0)
-                           + 2*self.f(t + self.dt/2, self.stage2(t))
-                           + 2*self.f(t + self.dt/2, self.stage3(t))
-                           + self.f(t + self.dt, self.stage4(t)))
+            + self.dt/6 * (self.f(t, self.u0, self.uh)
+                           + 2*self.f(t + self.dt/2, self.stage2(t), self.uh)
+                           + 2*self.f(t + self.dt/2, self.stage3(t), self.uh)
+                           + self.f(t + self.dt, self.stage4(t), self.uh))
 
     def solve(self):
         u_n = self.u0
@@ -33,37 +34,36 @@ class explicit_RungeKutta(object):
             u_n = self.stagef(t)
             self.u0 = u_n
             yield u_n
-    
+
     def solution(self):
         return np.array(tuple(self.solve()))
 
 
+# t0, te = 0, 1.
+# tol_newton = 1e-9
+# tol_sol = 1e-5
+# N = 5
+# t = 0
 
-t0, te = 0, 1.
-tol_newton = 1e-9
-tol_sol = 1e-5
-N = 5
-t = 0
+# u0 = np.array([2., 3.])
+# M = np.array([[0, 1], [-1, 0]])
+# def f(t,y): return np.dot(M, y)
+# system = explicit_RungeKutta(f, u0, t0, te, N)
 
-u0 = np.array([2., 3.])
-M = np.array([[0, 1], [-1, 0]])
-def f(t,y): return np.dot(M, y)
-system = explicit_RungeKutta(f, u0, t0, te, N)
+# timegrid  = np.linspace(t0, te, N+2)
+# def exact_sol(timegrid):
+#     for t in timegrid:
+#         y1 = np.array([2*np.cos(t) + 3*np.sin(t)])
+#         y2 = np.array([-2*np.sin(t)+ 3*np.cos(t)])
+#         yield np.hstack((y1,y2))
 
-timegrid  = np.linspace(t0, te, N+2)
-def exact_sol(timegrid):
-    for t in timegrid:
-        y1 = np.array([2*np.cos(t) + 3*np.sin(t)])
-        y2 = np.array([-2*np.sin(t)+ 3*np.cos(t)])
-        yield np.hstack((y1,y2))
-
-system.solve()
-S = system.solution()
-print(S)
-# t = S[:, 0]
-# y = S[:, 1]
-a = np.vstack(tuple(exact_sol(timegrid)))
-print(a)
+# system.solve()
+# S = system.solution()
+# print(S)
+# # t = S[:, 0]
+# # y = S[:, 1]
+# a = np.vstack(tuple(exact_sol(timegrid)))
+# print(a)
 # error = np.linalg.norm(np.vstack(y)-a, axis=-1)
 # plt.plot(np.vstack(t), error)
 # plt.show()
--- function_Fm.py
+++ function_Fm.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from explicit_RK import *
 from expressions import *
 
 Mb = np.array([
@@ -16,21 +17,36 @@ Mb = np.array([
 
 poly_b = np.array([[-1, -1, 1], [1/2, 3/2, -1], [3/2, 1/8, -3/8]])
 x = np.array([0.16, .093])
-npnts = 3
+npnts = 1
 
 uh = assembled_matrix(Mb, npnts, 2, 1, poly_b=poly_b)
 X0 = uh.X_0()
 
 
-def Fm(uh, X0):
+def Fm(t, X0, uh):
+    F = []
     for xn in uh.Mi:
         uh.x = xn
-        yield uh.F_m(X0)[0], uh.J()[0]
+        # yield uh.F_m(X0)[0], uh.J()[0]
+        F.append(uh.F_m(X0)[0])
+    return np.vstack(F)
+
+# F, J = Fm(uh, X0)
+# F, J = np.vstack(F), np.vstack(J)
+# print(J)
 
 
-FJ = np.hstack(tuple(Fm(uh, X0))) #FJ
-F, J = np.vstack(FJ[::2]), np.vstack(FJ[1::2])
+t0, te = 0, 1.
+N = 10
 
-
-#print(np.vstack([F]*2))
-print(J)
+system = explicit_RungeKutta(Fm, X0, t0, te, N, uh)
+system.solve()
+S = system.solution()
+print(S)
+# # t = S[:, 0]
+# # y = S[:, 1]
+# a = np.vstack(tuple(exact_sol(timegrid)))
+# print(a)
+# error = np.linalg.norm(np.vstack(y)-a, axis=-1)
+# plt.plot(np.vstack(t), error)
+# plt.show()
