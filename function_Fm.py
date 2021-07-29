734ec64db4c7dd5d52706abafcd0bcb48e0cecf3
Johny Vallejo <jhonyal58@gmail.com>
2021-07-29 01:00:05 -0500

    Update

--- function_Fm.py
+++ function_Fm.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from explicit_RK import *
 from expressions import *
+from halton_points import HaltonPoints
 
 
 def Fm(t, X0, uh):
@@ -14,16 +15,27 @@ def Fm(t, X0, uh):
 
 
 Mb = np.array([
-    [0., 0.],
+    #[0., 0.],
     [0., 0.5],
-    [0., 1.],
+    #[0., 1.],
     [0.5, 1.],
-    [1., 1.],
+    #[1., 1.],
     [1., 0.5],
-    [1., 0.],
+    #[1., 0.],
     [0.5, 0.]
 ])
+nf = 20
+r = HaltonPoints(2, nf).haltonPoints()
+fxl = r
+fxl[:,0] = 0
+fxr = r
+fxr[:,1] = 1
+fyu = r
+fyu[0,:] = 0
+fyd = r
+fyd[1,:] = 1
 
+Mb = np.vstack((fxl, fxr, fyu, fyd))
 
 poly_b = np.array([[-1, -1, 1], [1/2, 3/2, -1], [3/2, 1/8, -3/8]])
 npnts = 2
@@ -48,7 +60,8 @@ o2 = uh.O2()
 K = np.vstack((np.hstack((k1, m, q1)), np.hstack((m.T, k2, q2)), np.hstack((q1.T, q2.T, o2))))
 
 A = np.vstack((np.hstack((k2, q2)), np.hstack((q2.T, o2))))
-print(np.linalg.det(K))
+_, s, __ = np.linalg.svd(A)
+print('Condition Number: {:,.2f}'.format(max(s)/min(s)))
 #print(S[:, 0])
 # # t = S[:, 0]
 # # y = S[:, 1]
